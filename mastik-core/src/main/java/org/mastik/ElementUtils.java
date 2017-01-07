4208e383af6251bf0076435c2554d18056379a27
Imri Hecht <imriqwe@gmail.com>
2017-01-07 13:24:15 +0200

    Added missing ElementUtils

---
+++ mastik-core/src/main/java/org/mastik/ElementUtils.java
@@ -0,0 +1,60 @@
+package org.mastik;
+
+import org.apache.tinkerpop.gremlin.process.traversal.P;
+import org.apache.tinkerpop.gremlin.process.traversal.step.util.HasContainer;
+import org.apache.tinkerpop.gremlin.structure.*;
+import org.mastik.query.PredicatesTree;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Utility functions related to elements
+ *
+ * @author imriqwe (imriqwe@gmail.com)
+ * @since 1/6/17
+ */
+public class ElementUtils {
+    /**
+     * Given an edge of a vertex, extracts the vertex residing in the given direction from the edge
+     */
+    public static Vertex extractVertexFromEdge(Edge edge, Vertex vertex, Direction direction) {
+        switch (direction) {
+            case OUT:
+                return edge.inVertex();
+            case IN:
+                return edge.outVertex();
+            case BOTH:
+                Vertex outV = edge.outVertex();
+                Vertex inV = edge.inVertex();
+                if (vertex.id().equals(outV.id()))
+                    return inV;
+                return outV;
+            default:
+                throw new IllegalArgumentException(String.format("Direction '%s' is unsupported", direction.toString()));
+        }
+    }
+
+    /**
+     * Given a set of element ids, creates a predicate-tree with a predicate containing them
+     * @param ids Set of element ids
+     * @return New instance of predicate-tree with ids predicate
+     */
+    public static PredicatesTree createIdsPredicate(Set<Object> ids) {
+        if (ids.isEmpty()) {
+            return PredicatesTree.emptyTree();
+        }
+
+        List<Object> extractedIds = ids.stream()
+                .map(id -> {
+                    if (id instanceof Element)
+                        return ((Element) id).id();
+                    return id;
+                })
+                .collect(Collectors.toList());
+
+        HasContainer idPredicate = new HasContainer(T.id.getAccessor(), P.within(extractedIds));
+        return PredicatesTree.createFromPredicates(idPredicate);
+    }
+}
